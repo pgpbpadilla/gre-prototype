273c68292599841b9ce7a09487e89871ba773423
Pablo G. Padilla <ppadilla@aserta.com.mx>
2013-03-17 19:35:38 -0600

    Fix referencies

--- src/main/webapp/resources/js/PruebasServicios/utils.js
+++ src/main/webapp/resources/js/PruebasServicios/utils.js
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+// TODO: Remove this file
 
 function setupView(){
     $('.node-info').click(function(){
