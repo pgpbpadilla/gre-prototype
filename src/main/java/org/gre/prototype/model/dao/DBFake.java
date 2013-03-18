cdf30d646d5ee20ae41564e4c6d3f4a65e673561
Pablo G. Padilla <ppadilla@aserta.com.mx>
2013-03-17 18:29:26 -0600

    Fix: import

--- src/main/java/org/gre/prototype/model/dao/DBFake.java
+++ src/main/java/org/gre/prototype/model/dao/DBFake.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.model.dao;
+package org.gre.prototype.model.dao;
 
 import com.model.pojos.*;
 import java.util.ArrayList;
